dba933396170e9c4b583f02f87cc46dec1efccc5
Felicia Garcia <>
2018-10-22 17:33:45 -0400

    dark sky api call

---
+++ app/src/main/java/com/feliciag/fun_app/MainActivity.java
@@ -0,0 +1,80 @@
+package com.feliciag.fun_app;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button weatherButton = findViewById(R.id.weather_button);
+
+        mQueue = Volley.newRequestQueue(this );
+
+        weatherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+
+
+            }
+        });
+    }
+
+    private void jsonParse() {
+
+        String url = "https://api.darksky.net/forecast/dc27a31d812ea41311c015b809abeddc/17.9557,77.2405";
+
+        JsonObjectRequest request = new JsonObjectRequest(com.android.volley.Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("hourly");
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject hourly = jsonArray.getJSONObject(i);
+
+                                String summary = hourly.getString("summary");
+
+                                mTextViewResult.append(summary);
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        mQueue.add(request);
+    }
+}
